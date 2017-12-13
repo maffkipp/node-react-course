a57b405febf4628c22e05f6f330452d0dc76970d
Wesley Maffly-Kipp <maffkipp@yahoo.com>
2017-12-13 11:41:09 -0600

    added mongoose, user schema

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+require('./models/User');
 require('./services/passport');
+
+mongoose.connect(keys.mongoURI, { useMongoClient: true });
 
 const app = express();
 
 require('./routes/authRoutes')(app);
 
-
-
 const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.send('this is the home page');
+});
 
 app.listen(PORT);
