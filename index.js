bf34940932a50b32ef7a05916e88113fbb6eec77
Wesley Maffly-Kipp <maffkipp@yahoo.com>
2017-12-13 15:44:21 -0600

    finished auth flow

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
@@ -8,12 +10,17 @@ mongoose.connect(keys.mongoURI, { useMongoClient: true });
 
 const app = express();
 
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
 require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
-
-app.get('/', (req, res) => {
-  res.send('this is the home page');
-});
 
 app.listen(PORT);
